1e0fa652fcfb51c58a34cb3fee23de444781e4da
gsstoykov <georgi.stoykov@limechain.tech>
2025-07-15 14:22:23 +0300

    feat: Add recid calculation and jumbo test
    Signed-off-by: gsstoykov <georgi.stoykov@limechain.tech>

--- src/sdk/tests/integration/EthereumTransactionIntegrationTests.cc
+++ src/sdk/tests/integration/EthereumTransactionIntegrationTests.cc
@@ -1,26 +1,20 @@
 // SPDX-License-Identifier: Apache-2.0
-#include "AccountCreateTransaction.h"
-#include "AccountDeleteTransaction.h"
 #include "AccountInfo.h"
 #include "AccountInfoQuery.h"
 #include "BaseIntegrationTest.h"
 #include "Client.h"
 #include "ContractCreateTransaction.h"
-#include "ContractDeleteTransaction.h"
 #include "ContractFunctionParameters.h"
 #include "ContractId.h"
 #include "ECDSAsecp256k1PrivateKey.h"
 #include "ECDSAsecp256k1PublicKey.h"
-#include "ED25519PrivateKey.h"
 #include "EthereumTransaction.h"
 #include "FileCreateTransaction.h"
-#include "FileDeleteTransaction.h"
 #include "FileId.h"
 #include "TransactionReceipt.h"
 #include "TransactionRecord.h"
 #include "TransactionResponse.h"
 #include "TransferTransaction.h"
-#include "exceptions/OpenSSLException.h"
 #include "impl/HexConverter.h"
 #include "impl/RLPItem.h"
 #include "impl/Utilities.h"
@@ -35,7 +29,7 @@ class EthereumTransactionIntegrationTests : public BaseIntegrationTest
 };
 
 //-----
-TEST_F(EthereumTransactionIntegrationTests, DISABLED_SignerNonceChangedOnEthereumTransaction)
+TEST_F(EthereumTransactionIntegrationTests, SignerNonceChangedOnEthereumTransaction)
 {
   // Given
   const std::shared_ptr<ECDSAsecp256k1PrivateKey> testPrivateKey = ECDSAsecp256k1PrivateKey::fromString(
@@ -70,7 +64,7 @@ TEST_F(EthereumTransactionIntegrationTests, DISABLED_SignerNonceChangedOnEthereu
                     ContractCreateTransaction()
                       .setBytecodeFileId(fileId)
                       .setAdminKey(getTestClient().getOperatorPublicKey())
-                      .setGas(200000ULL)
+                      .setGas(2000000ULL)
                       .setConstructorParameters(ContractFunctionParameters().addString("Hello from Hiero.").toBytes())
                       .setMemo(memo)
                       .execute(getTestClient())
@@ -89,11 +83,12 @@ TEST_F(EthereumTransactionIntegrationTests, DISABLED_SignerNonceChangedOnEthereu
   std::vector<std::byte> callData = ContractFunctionParameters().addString("new message").toBytes("setMessage");
   std::vector<std::byte> accessList = {};
 
+  // When
   // Serialize bytes to RLP format for signing
   RLPItem list(RLPItem::RLPType::LIST_TYPE);
   list.pushBack(chainId);
   list.pushBack(RLPItem());
-  list.pushBack(maxPriorityGas);
+  list.pushBack(RLPItem());
   list.pushBack(maxGas);
   list.pushBack(gasLimit);
   list.pushBack(to);
@@ -113,25 +108,125 @@ TEST_F(EthereumTransactionIntegrationTests, DISABLED_SignerNonceChangedOnEthereu
   std::vector<std::byte> s(signedBytes.end() - std::min(signedBytes.size(), static_cast<size_t>(32)),
                            signedBytes.end());
 
-  std::vector<std::byte> recoveryId = internal::HexConverter::hexToBytes("01");
+  // Calculate recovery id
+  int recId =
+    testPrivateKey->calculateRecoveryId(internal::Utilities::concatenateVectors({ type, list.write() }), r, s);
+  ASSERT_NE(recId, -1) << "Failed to calculate recovery id";
+  std::vector<std::byte> recoveryId = { static_cast<std::byte>(recId) };
 
-  // recId, r, s should be added to original RLP list as Ethereum Transactions require
-  list.pushBack(recoveryId);
-  list.pushBack(r);
-  list.pushBack(s);
+  RLPItem testList = list;
+  testList.pushBack(recoveryId);
+  testList.pushBack(r);
+  testList.pushBack(s);
 
-  std::vector<std::byte> ethereumTransactionData = list.write();
-  // Type should be concatenated to RLP as this is a service side requirement
+  std::vector<std::byte> ethereumTransactionData = testList.write();
   ethereumTransactionData = internal::Utilities::concatenateVectors({ type, ethereumTransactionData });
 
-  // When Then
   EthereumTransaction ethereumTransaction;
   EXPECT_NO_THROW(ethereumTransaction = EthereumTransaction().setEthereumData(ethereumTransactionData));
-
   TransactionResponse txResponse;
   EXPECT_NO_THROW(txResponse = ethereumTransaction.execute(getTestClient()));
-
   EXPECT_TRUE(txResponse.getRecord(getTestClient()).mContractFunctionResult.has_value());
-  //  mSignerNonce should be incremented to 1 after the first contract execution
+  EXPECT_EQ(txResponse.getRecord(getTestClient()).mContractFunctionResult.value().mSignerNonce, 1);
+}
+
+//-----
+TEST_F(EthereumTransactionIntegrationTests, JumboTransaction)
+{
+  // Given
+  const std::shared_ptr<ECDSAsecp256k1PrivateKey> testPrivateKey = ECDSAsecp256k1PrivateKey::fromString(
+    "30540201010420ac318ea8ff8d991ab2f16172b4738e74dc35a56681199cfb1c0cb2e7cb560ffda00706052b8104000aa124032200036843f5"
+    "cb338bbb4cdb21b0da4ea739d910951d6e8a5f703d313efe31afe788f4");
+  const std::shared_ptr<ECDSAsecp256k1PublicKey> testPublicKey =
+    std::dynamic_pointer_cast<ECDSAsecp256k1PublicKey>(testPrivateKey->getPublicKey());
+  const AccountId aliasAccountId = testPublicKey->toAccountId();
+
+  TransactionReceipt aliasTransferTxReciept;
+  EXPECT_NO_THROW(aliasTransferTxReciept =
+                    TransferTransaction()
+                      .addHbarTransfer(getTestClient().getOperatorAccountId().value(), Hbar(1LL).negated())
+                      .addHbarTransfer(aliasAccountId, Hbar(1LL))
+                      .execute(getTestClient())
+                      .getReceipt(getTestClient()));
+
+  AccountInfo accountInfo;
+  EXPECT_NO_THROW(accountInfo = AccountInfoQuery().setAccountId(aliasAccountId).execute(getTestClient()));
+
+  FileId fileId;
+  EXPECT_NO_THROW(fileId = FileCreateTransaction()
+                             .setKeys({ getTestClient().getOperatorPublicKey() })
+                             .setContents(internal::Utilities::stringToByteVector(getTestJumboSmartContractBytecode()))
+                             .execute(getTestClient())
+                             .getReceipt(getTestClient())
+                             .mFileId.value());
+
+  ContractId contractId;
+  EXPECT_NO_THROW(contractId = ContractCreateTransaction()
+                                 .setBytecodeFileId(fileId)
+                                 .setAdminKey(getTestClient().getOperatorPublicKey())
+                                 .setGas(2000000ULL)
+                                 .execute(getTestClient())
+                                 .getReceipt(getTestClient())
+                                 .mContractId.value());
+
+  std::vector<std::byte> type = internal::HexConverter::hexToBytes("02");
+  std::vector<std::byte> chainId = internal::HexConverter::hexToBytes("012a");
+  std::vector<std::byte> nonce = internal::HexConverter::hexToBytes("00");
+  std::vector<std::byte> maxPriorityGas = internal::HexConverter::hexToBytes("00");
+  std::vector<std::byte> maxGas = internal::HexConverter::hexToBytes("d1385c7bf0");
+  std::vector<std::byte> gasLimit = internal::HexConverter::hexToBytes("3567e0");
+  std::vector<std::byte> to = internal::HexConverter::hexToBytes(contractId.toSolidityAddress());
+  std::vector<std::byte> value = internal::HexConverter::hexToBytes("00");
+  std::vector<std::byte> jumboCallData(1024 * 120, std::byte(0));
+  std::vector<std::byte> callData =
+    ContractFunctionParameters().addBytes(jumboCallData).toBytes("consumeLargeCalldata");
+
+  callData.insert(callData.end(), 32, std::byte(0));
+  std::vector<std::byte> accessList = {};
+
+  // When
+  // Serialize bytes to RLP format for signing
+  RLPItem list(RLPItem::RLPType::LIST_TYPE);
+  list.pushBack(chainId);
+  list.pushBack(RLPItem());
+  list.pushBack(RLPItem());
+  list.pushBack(maxGas);
+  list.pushBack(gasLimit);
+  list.pushBack(to);
+  list.pushBack(RLPItem());
+  list.pushBack(callData);
+  RLPItem accessListItem(accessList);
+  accessListItem.setType(RLPItem::RLPType::LIST_TYPE);
+  list.pushBack(accessListItem);
+
+  // signed bytes in r,s form
+  std::vector<std::byte> signedBytes =
+    testPrivateKey->sign(internal::Utilities::concatenateVectors({ type, list.write() }));
+
+  std::vector<std::byte> r(signedBytes.begin(),
+                           signedBytes.begin() + std::min(signedBytes.size(), static_cast<size_t>(32)));
+
+  std::vector<std::byte> s(signedBytes.end() - std::min(signedBytes.size(), static_cast<size_t>(32)),
+                           signedBytes.end());
+
+  // Calculate recovery id
+  int recId =
+    testPrivateKey->calculateRecoveryId(internal::Utilities::concatenateVectors({ type, list.write() }), r, s);
+  ASSERT_NE(recId, -1) << "Failed to calculate recovery id";
+  std::vector<std::byte> recoveryId = { static_cast<std::byte>(recId) };
+
+  RLPItem testList = list;
+  testList.pushBack(recoveryId);
+  testList.pushBack(r);
+  testList.pushBack(s);
+
+  std::vector<std::byte> ethereumTransactionData = testList.write();
+  ethereumTransactionData = internal::Utilities::concatenateVectors({ type, ethereumTransactionData });
+
+  EthereumTransaction ethereumTransaction;
+  EXPECT_NO_THROW(ethereumTransaction = EthereumTransaction().setEthereumData(ethereumTransactionData));
+  TransactionResponse txResponse;
+  EXPECT_NO_THROW(txResponse = ethereumTransaction.execute(getTestClient()));
+  EXPECT_TRUE(txResponse.getRecord(getTestClient()).mContractFunctionResult.has_value());
   EXPECT_EQ(txResponse.getRecord(getTestClient()).mContractFunctionResult.value().mSignerNonce, 1);
 }
