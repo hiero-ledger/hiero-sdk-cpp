35452c9bfdd122c0eef1e17c6d7ffd2aeb550cbc
Rob Walworth <robert.walworth@swirldslabs.com>
2025-10-13 22:49:46 -0400

    feat: add integration tests for hooks
    Signed-off-by: Rob Walworth <robert.walworth@swirldslabs.com>

--- src/sdk/tests/integration/AccountCreateTransactionIntegrationTests.cc
+++ src/sdk/tests/integration/AccountCreateTransactionIntegrationTests.cc
@@ -28,6 +28,58 @@ using namespace Hiero;
 
 class AccountCreateTransactionIntegrationTests : public BaseIntegrationTest
 {
+protected:
+  void SetUp() override
+  {
+    mContractId =
+      ContractCreateTransaction()
+        .setBytecode(internal::HexConverter::hexToBytes("6080604052348015600e575f5ffd5b506103da8061001c5f395ff3fe608060"
+                                                        "40526004361061001d575f3560e01c80630b6c5c0414610021"
+                                                        "575b5f5ffd5b61003b6004803603810190610036919061021c565b61005156"
+                                                        "5b60405161004891906102ed565b60405180910390f35b5f61"
+                                                        "016d73ffffffffffffffffffffffffffffffffffffffff163073ffffffffff"
+                                                        "ffffffffffffffffffffffffffffff16146100c2576040517f"
+                                                        "08c379a0000000000000000000000000000000000000000000000000000000"
+                                                        "0081526004016100b990610386565b60405180910390fd5b60"
+                                                        "019050979650505050505050565b5f5ffd5b5f5ffd5b5f73ffffffffffffff"
+                                                        "ffffffffffffffffffffffffff82169050919050565b5f6101"
+                                                        "02826100d9565b9050919050565b610112816100f8565b811461011c575f5f"
+                                                        "fd5b50565b5f8135905061012d81610109565b92915050565b"
+                                                        "5f819050919050565b61014581610133565b811461014f575f5ffd5b50565b"
+                                                        "5f813590506101608161013c565b92915050565b5f5ffd5b5f"
+                                                        "5ffd5b5f5ffd5b5f5f83601f84011261018757610186610166565b5b823590"
+                                                        "5067ffffffffffffffff8111156101a4576101a361016a565b"
+                                                        "5b6020830191508360018202830111156101c0576101bf61016e565b5b9250"
+                                                        "929050565b5f5f83601f8401126101dc576101db610166565b"
+                                                        "5b8235905067ffffffffffffffff8111156101f9576101f861016a565b5b60"
+                                                        "20830191508360018202830111156102155761021461016e56"
+                                                        "5b5b9250929050565b5f5f5f5f5f5f5f60a0888a0312156102375761023661"
+                                                        "00d1565b5b5f6102448a828b0161011f565b97505060206102"
+                                                        "558a828b01610152565b96505060406102668a828b01610152565b95505060"
+                                                        "6088013567ffffffffffffffff811115610287576102866100"
+                                                        "d5565b5b6102938a828b01610172565b9450945050608088013567ffffffff"
+                                                        "ffffffff8111156102b6576102b56100d5565b5b6102c28a82"
+                                                        "8b016101c7565b925092505092959891949750929550565b5f811515905091"
+                                                        "9050565b6102e7816102d3565b82525050565b5f6020820190"
+                                                        "506103005f8301846102de565b92915050565b5f8282526020820190509291"
+                                                        "5050565b7f436f6e74726163742063616e206f6e6c79206265"
+                                                        "2063616c6c656420617320615f8201527f20686f6f6b000000000000000000"
+                                                        "00000000000000000000000000000000000060208201525056"
+                                                        "5b5f610370602583610306565b915061037b82610316565b60408201905091"
+                                                        "9050565b5f6020820190508181035f83015261039d81610364"
+                                                        "565b905091905056fea2646970667358221220a8c76458204f8bb9a86f59ec"
+                                                        "2f0ccb7cbe8ae4dcb65700c4b6ee91a39404083a64736f6c63"
+                                                        "4300081e0033"))
+        .setGas(300000ULL)
+        .execute(getTestClient())
+        .getReceipt(getTestClient())
+        .mContractId.value();
+  }
+
+  [[nodiscard]] const ContractId& getTestContractId() const { return mContractId; }
+
+private:
+  ContractId mContractId;
 };
 
 //-----
@@ -644,4 +696,126 @@ TEST_F(AccountCreateTransactionIntegrationTests, CreateTransactionWithAliasAndKe
   EXPECT_NO_THROW(txReceipt = txResponse.getReceipt(getTestClient()));
 
   ASSERT_EQ(txReceipt.mAccountId.has_value(), true);
+}
+
+//-----
+TEST_F(AccountCreateTransactionIntegrationTests, CreateTransactionWithLambdaHook)
+{
+  // Given
+  const std::shared_ptr<ECDSAsecp256k1PrivateKey> ecdsaPrivateKey = ECDSAsecp256k1PrivateKey::generatePrivateKey();
+
+  LambdaEvmHook lambdaEvmHook;
+  lambdaEvmHook.setContractId(getTestContractId());
+
+  HookCreationDetails hookCreationDetails;
+  hookCreationDetails.setExtensionPoint(HookExtensionPoint::ACCOUNT_ALLOWANCE_HOOK);
+  hookCreationDetails.setHookId(1LL);
+  hookCreationDetails.setLambdaEvmHook(lambdaEvmHook);
+
+  // When / Then
+  TransactionResponse txResponse;
+  EXPECT_NO_THROW(txResponse = AccountCreateTransaction()
+                                 .setKeyWithoutAlias(ecdsaPrivateKey)
+                                 .addHook(hookCreationDetails)
+                                 .freezeWith(&getTestClient())
+                                 .sign(ecdsaPrivateKey)
+                                 .execute(getTestClient()));
+
+  TransactionReceipt txReceipt;
+  EXPECT_NO_THROW(txReceipt = txResponse.getReceipt(getTestClient()));
+
+  EXPECT_TRUE(txReceipt.mAccountId.has_value());
+}
+
+//-----
+TEST_F(AccountCreateTransactionIntegrationTests, CreateTransactionWithLambdaHookAndStorageUpdates)
+{
+  // Given
+  const std::shared_ptr<ECDSAsecp256k1PrivateKey> ecdsaPrivateKey = ECDSAsecp256k1PrivateKey::generatePrivateKey();
+
+  LambdaEvmHook lambdaEvmHook;
+  lambdaEvmHook.setContractId(getTestContractId());
+
+  LambdaStorageSlot lambdaStorageSlot;
+  lambdaStorageSlot.setKey({ std::byte(0x01), std::byte(0x23), std::byte(0x45) });
+  lambdaStorageSlot.setValue({ std::byte(0x67), std::byte(0x89), std::byte(0xAB) });
+
+  LambdaStorageUpdate lambdaStorageUpdate;
+  lambdaStorageUpdate.setStorageSlot(lambdaStorageSlot);
+
+  lambdaEvmHook.addStorageUpdate(lambdaStorageUpdate);
+
+  HookCreationDetails hookCreationDetails;
+  hookCreationDetails.setExtensionPoint(HookExtensionPoint::ACCOUNT_ALLOWANCE_HOOK);
+  hookCreationDetails.setHookId(1LL);
+  hookCreationDetails.setLambdaEvmHook(lambdaEvmHook);
+
+  // When / Then
+  TransactionResponse txResponse;
+  EXPECT_NO_THROW(txResponse = AccountCreateTransaction()
+                                 .setKeyWithoutAlias(ecdsaPrivateKey)
+                                 .addHook(hookCreationDetails)
+                                 .freezeWith(&getTestClient())
+                                 .sign(ecdsaPrivateKey)
+                                 .execute(getTestClient()));
+
+  TransactionReceipt txReceipt;
+  EXPECT_NO_THROW(txReceipt = txResponse.getReceipt(getTestClient()));
+
+  EXPECT_TRUE(txReceipt.mAccountId.has_value());
+}
+
+//-----
+TEST_F(AccountCreateTransactionIntegrationTests, CreateTransactionWithLambdaHookWithNoContractId)
+{
+  // Given
+  const std::shared_ptr<ECDSAsecp256k1PrivateKey> ecdsaPrivateKey = ECDSAsecp256k1PrivateKey::generatePrivateKey();
+
+  LambdaEvmHook lambdaEvmHook;
+
+  LambdaStorageSlot lambdaStorageSlot;
+  lambdaStorageSlot.setKey({ std::byte(0x01), std::byte(0x23), std::byte(0x45) });
+  lambdaStorageSlot.setValue({ std::byte(0x67), std::byte(0x89), std::byte(0xAB) });
+
+  LambdaStorageUpdate lambdaStorageUpdate;
+  lambdaStorageUpdate.setStorageSlot(lambdaStorageSlot);
+
+  lambdaEvmHook.addStorageUpdate(lambdaStorageUpdate);
+
+  HookCreationDetails hookCreationDetails;
+  hookCreationDetails.setExtensionPoint(HookExtensionPoint::ACCOUNT_ALLOWANCE_HOOK);
+  hookCreationDetails.setHookId(1LL);
+  hookCreationDetails.setLambdaEvmHook(lambdaEvmHook);
+
+  // When / Then
+  EXPECT_THROW(AccountCreateTransaction()
+                 .setKeyWithoutAlias(ecdsaPrivateKey)
+                 .addHook(hookCreationDetails)
+                 .execute(getTestClient())
+                 .getReceipt(getTestClient()),
+               PrecheckStatusException); // INVALID_MAX_AUTO_ASSOCIATIONS
+}
+
+//-----
+TEST_F(AccountCreateTransactionIntegrationTests, CreateTransactionWithSameLambdaHookIds)
+{
+  // Given
+  const std::shared_ptr<ECDSAsecp256k1PrivateKey> ecdsaPrivateKey = ECDSAsecp256k1PrivateKey::generatePrivateKey();
+
+  LambdaEvmHook lambdaEvmHook;
+  lambdaEvmHook.setContractId(getTestContractId());
+
+  HookCreationDetails hookCreationDetails;
+  hookCreationDetails.setExtensionPoint(HookExtensionPoint::ACCOUNT_ALLOWANCE_HOOK);
+  hookCreationDetails.setHookId(1LL);
+  hookCreationDetails.setLambdaEvmHook(lambdaEvmHook);
+
+  // When / Then
+  EXPECT_THROW(AccountCreateTransaction()
+                 .setKeyWithoutAlias(ecdsaPrivateKey)
+                 .addHook(hookCreationDetails)
+                 .addHook(hookCreationDetails)
+                 .execute(getTestClient())
+                 .getReceipt(getTestClient()),
+               PrecheckStatusException); // INVALID_MAX_AUTO_ASSOCIATIONS
 }
