d83f8d1e98b6e46458c1389e823f115de5b7cf63
Jyotish <jyotishdeka091@gmail.com>
2026-02-04 03:15:06 +0000

    Fix formatting issues

--- src/sdk/main/include/TokenUpdateTransaction.h
+++ src/sdk/main/include/TokenUpdateTransaction.h
@@ -325,6 +325,7 @@ public:
    * @return The new metadata for the token.
    */
   [[nodiscard]] inline std::vector<std::byte> getMetadata() const { return mMetadata; }
+ 
   /**
    * Get the token verification mode for the token.
    *
