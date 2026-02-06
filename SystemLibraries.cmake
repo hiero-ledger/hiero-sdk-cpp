31c505b5b9712933ab283628f2f3db8fac11dc87
Jyotish <jyotishdeka091@gmail.com>
2026-02-06 20:49:14 +0530

    Implement Linux dependency checks in CMake
    Added checks for zip utility and Linux headers in CMake.
    
    Signed-off-by: Jyotish <jyotishdeka091@gmail.com>

--- SystemLibraries.cmake
+++ SystemLibraries.cmake
@@ -8,6 +8,26 @@ if(NOT WIN32)
     set_target_properties(sys::resolv PROPERTIES IMPORTED_LOCATION ${RESOLV_LIBRARY})
 endif()
 
+     # Linux dependency checks
+if(UNIX AND NOT APPLE)
+
+    # Check for zip utility
+    find_program(ZIP_EXECUTABLE zip)
+
+    if(NOT ZIP_EXECUTABLE)
+        message(FATAL_ERROR "zip utility is required but not installed. Run: sudo apt install zip")
+    endif()
+
+    # Check for linux headers (linux-libc-dev)
+    include(CheckIncludeFile)
+    check_include_file("linux/version.h" HAVE_LINUX_HEADERS)
+
+    if(NOT HAVE_LINUX_HEADERS)
+        message(FATAL_ERROR "linux-libc-dev is required but not installed. Run: sudo apt install linux-libc-dev")
+    endif()
+
+endif()
+
 if(APPLE)
     if(NOT DEFINED OSX_CORE_FOUNDATION)
         find_library(OSX_CORE_FOUNDATION CoreFoundation)
