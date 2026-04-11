5298ff81156d732a3a8fc8683217a4d6f13a284c
Omswastik-11 <omswastikpanda11@gmail.com>
2026-04-11 22:19:23 +0530

    feat: Add operator== to AccountBalance class
    Signed-off-by: Omswastik-11 <omswastikpanda11@gmail.com>

--- src/sdk/tests/unit/AccountBalanceUnitTests.cc
+++ src/sdk/tests/unit/AccountBalanceUnitTests.cc
@@ -28,3 +28,108 @@ TEST_F(AccountBalanceUnitTests, DeserializeAccountBalanceFromProtobuf)
   // Then
   EXPECT_EQ(accountBalance.mBalance, getTestBalance());
 }
+
+//-----
+TEST_F(AccountBalanceUnitTests, ToString)
+{
+  // Given
+  AccountBalance accountBalance;
+  accountBalance.mBalance = getTestBalance();
+
+  // When
+  const std::string result = accountBalance.toString();
+
+  // Then
+  EXPECT_FALSE(result.empty());
+  EXPECT_NE(result.find(getTestBalance().toString()), std::string::npos);
+}
+
+//-----
+TEST_F(AccountBalanceUnitTests, DefaultEqualityOperator)
+{
+  // Given
+  AccountBalance accountBalance1;
+  AccountBalance accountBalance2;
+
+  // When
+  const bool areEqual = (accountBalance1 == accountBalance2);
+
+  // Then
+  EXPECT_TRUE(areEqual);
+}
+
+//-----
+TEST_F(AccountBalanceUnitTests, EqualityOperatorWithSameBalance)
+{
+  // Given
+  AccountBalance accountBalance1;
+  accountBalance1.mBalance = getTestBalance();
+
+  AccountBalance accountBalance2;
+  accountBalance2.mBalance = getTestBalance();
+
+  // When
+  const bool areEqual = (accountBalance1 == accountBalance2);
+
+  // Then
+  EXPECT_TRUE(areEqual);
+}
+
+//-----
+TEST_F(AccountBalanceUnitTests, EqualityOperatorWithDifferentBalance)
+{
+  // Given
+  AccountBalance accountBalance1;
+  accountBalance1.mBalance = getTestBalance();
+
+  AccountBalance accountBalance2;
+  accountBalance2.mBalance = Hbar(200LL);
+
+  // When
+  const bool areEqual = (accountBalance1 == accountBalance2);
+
+  // Then
+  EXPECT_FALSE(areEqual);
+}
+
+//-----
+TEST_F(AccountBalanceUnitTests, EqualityOperatorWithSameTokens)
+{
+  // Given
+  TokenId tokenId(0, 0, 1);
+
+  AccountBalance accountBalance1;
+  accountBalance1.mBalance = getTestBalance();
+  accountBalance1.mTokens[tokenId] = 50;
+
+  AccountBalance accountBalance2;
+  accountBalance2.mBalance = getTestBalance();
+  accountBalance2.mTokens[tokenId] = 50;
+
+  // When
+  const bool areEqual = (accountBalance1 == accountBalance2);
+
+  // Then
+  EXPECT_TRUE(areEqual);
+}
+
+//-----
+TEST_F(AccountBalanceUnitTests, EqualityOperatorWithDifferentTokens)
+{
+  // Given
+  TokenId tokenId(0, 0, 1);
+
+  AccountBalance accountBalance1;
+  accountBalance1.mBalance = getTestBalance();
+  accountBalance1.mTokens[tokenId] = 50;
+
+  AccountBalance accountBalance2;
+  accountBalance2.mBalance = getTestBalance();
+  accountBalance2.mTokens[tokenId] = 100;
+
+  // When
+  const bool areEqual = (accountBalance1 == accountBalance2);
+
+  // Then
+  EXPECT_FALSE(areEqual);
+}
