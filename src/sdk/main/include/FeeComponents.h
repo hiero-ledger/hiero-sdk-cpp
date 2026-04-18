937481dd942a6f23670de39492b90fa2aac4b7d5
Mouaad Datazout <datazoutmouaad@gmail.com>
2026-04-19 00:38:12 +0200

    add operator== to FeeComponents with unit tests (#1438)
    Signed-off-by: Mouaad Datazout <datazoutmouaad@gmail.com>

--- src/sdk/main/include/FeeComponents.h
+++ src/sdk/main/include/FeeComponents.h
@@ -324,6 +324,15 @@ private:
    * The price of bandwidth for data retrieved from disk for a response, measured in bytes.
    */
   int64_t mResponseDiskByte = 0LL;
+
+  /**
+   * Compare two FeeComponents instances and determine if they represent an equivalent set of fees.
+   *
+   * @param lhs The left-hand side FeeComponents to compare.
+   * @param rhs The right-hand side FeeComponents to compare.
+   * @return \c TRUE if \p lhs and \p rhs represent equivalent fee component values, otherwise \c FALSE.
+   */
+  [[nodiscard]] friend bool operator==(const FeeComponents& lhs, const FeeComponents& rhs);
 };
 
 } // namespace Hiero
